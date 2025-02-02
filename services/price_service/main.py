397d02850b2e90fcfb487ada3b566a7e43fea295
evren <evren@haltia.ai>
2025-02-02 20:59:11 +0300

    port fix

--- services/price_service/main.py
+++ services/price_service/main.py
@@ -143,5 +143,5 @@ if __name__ == "__main__":
     
     # Start the API server with uvicorn.
     import uvicorn
-    port = int(os.getenv("PORT", 8082))
+    port = int(os.getenv("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
